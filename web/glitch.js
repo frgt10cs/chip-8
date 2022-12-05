4da2a6070c37df9790968b252aeed1d218d12606
frgt10cs <ivan_gavrilov_2016@bk.ru>
2022-12-05 23:14:06 +0300

    added glitch and blick effect, fixed draw bug

---
+++ web/glitch.js
@@ -0,0 +1,23 @@
+function randomInt(min, max) {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+const glitch = (canvas, ctx) => {    
+    let data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    let k1 = randomInt(1, 5);
+    let k2 = randomInt(5, 8);
+    for (let y = 0; y < data.height; y++) {
+        let deltaY = y * data.width;
+        let skew = Math.ceil((y % (16 * k1)) / k2)
+        for (let x = 0; x < data.width; x++) {
+            data.data[(deltaY + x) * 4] = data.data[(deltaY + (x + skew)) * 4];
+            data.data[(deltaY + x) * 4 + 1] = data.data[(deltaY + (x + skew)) * 4 + 1];
+            data.data[(deltaY + x) * 4 + 2] = data.data[(deltaY + (x + skew)) * 4 + 2];
+            data.data[(deltaY + x) * 4 + 3] = data.data[(deltaY + (x + skew)) * 4 + 3];
+        }
+    }
+    ctx.putImageData(data, 0, 0);
+    setTimeout(() => glitch(canvas, ctx), randomInt(300, 5000));
+}
+
+export { glitch };
--- web/index.js
+++ web/index.js
@@ -1,4 +1,5 @@
 import init, { load_rom, exec_cycle, get_display_memory, get_memory, get_current_opcode } from '../pkg/chip8.js';
+import { glitch } from './glitch.js';
 
 await init();
 
@@ -48,29 +49,23 @@ runGameBtn.onclick = async function () {
     }
 
     let display_memory = null;
-    let delay = 0;
+    let delay = 20;
     let prevTime = new Date();
     let curTime = null;
     let timeDiff = null;            
 
     mainCycle = setInterval(() => {
       exec_cycle();      
-      // console.log("[" + it + "] " + get_current_opcode());
       display_memory = get_display_memory();            
-
-      // it doesn't work
-      // ctx.clearRect(0, 0, canvas.width, canvas.height);          
+      
+      ctx.clearRect(0, 0, canvas.width, canvas.height);          
 
       ctx.fillStyle = "rgb(30, 109, 47)";
       for (let y = 0; y < 32; y++) {
         for (let x = 0; x < 64; x++) {
           if (display_memory[y * 64 + x] != 0) {      
             ctx.fillStyle = "rgb(30, 109, 47)";      
-            ctx.fillRect(x*20, y*20, (x + 1)*20, (y + 1)*20);            
-          }
-          else{
-            ctx.fillStyle = "rgb(0, 0, 0)";      
-            ctx.fillRect(x*20, y*20, (x + 1)*20, (y + 1)*20);
+            ctx.fillRect(x*20, y*20, 20, 20);            
           }
         }        
       }           
@@ -81,11 +76,4 @@ runGameBtn.onclick = async function () {
   }
 }
 
-function draw(x1, y1, x2, y2) {
-  ctx.fillStyle = "rgb(30, 109, 47)";
-  ctx.fillRect(x1 * 20, y1 * 20, x2 * 20, y2 * 20);
-}
-
-draw(63, 31, 64, 32);
-
-draw(0, 0, 1, 1);
+glitch(canvas, ctx);
